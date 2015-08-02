8be47e4fd7fe4fadb8b34fc91f4f4fbe6b279705
chriswait91 <chriswait91@gmail.com>
2015-08-02 21:30:08 +1000

    Adding angular-material css import to base and gulpfile Adding bower dependencies

--- backend/gulpfile.js
+++ backend/gulpfile.js
@@ -8,6 +8,7 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var templateCache = require('gulp-angular-templatecache');
+var vendor = require('gulp-concat-vendor');
 
 var angular_app_root = 'app';
 var angular_app_module_js = [angular_app_root + '/**/*.js', "!" + angular_app_root + '/templates.js'];
@@ -51,6 +52,19 @@ gulp.task('watch', function() {
     gulp.watch(angular_app_module_html, ['templates']);
     gulp.watch(angular_app_module_js, ['lint', 'scripts']);
     gulp.watch(angular_app_module_scss, ['sass']);
+});
+
+// Fetch bower JS dependencies
+gulp.task('bower', function() {
+    gulp.src('bower_components/*')
+        .pipe(vendor('vendor.js'))
+        .pipe(gulp.dest(django_static_path)); 
+});
+
+// Fetch bower JS dependencies
+gulp.task('material', function() {
+    gulp.src('bower_components/angular-material/angular-material.css')
+        .pipe(gulp.dest(django_static_path)); 
 });
 
 // Default Task
