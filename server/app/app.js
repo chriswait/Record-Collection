1adad1d476305e8a49824999d2e355b0279db989
chriswait91 <chriswait91@gmail.com>
2015-08-17 00:06:13 +1000

    replacing icons with ng-md-icon: * adding angular-material-icons in bower.json, app.js * replacing svg with ng-md-icon

--- server/app/app.js
+++ server/app/app.js
@@ -1,3 +1,3 @@
-var app = angular.module("recordApp", ['templates','ngMaterial']);
-app.controller("MainController", function($scope) {
+var app = angular.module("recordApp", ['templates','ngMaterial','ngMdIcons']);
+app.controller("MainController", function() {
 });
