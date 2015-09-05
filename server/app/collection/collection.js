159c25e2f02aa3888bad1da0c6083da6e0dd1600
chriswait91 <chriswait91@gmail.com>
2015-09-06 00:27:28 +1000

    Adding searching, sorting and filtering collection

--- server/app/collection/collection.js
+++ server/app/collection/collection.js
@@ -44,7 +44,6 @@ angular.module("recordApp")
 })
 .controller("CollectionController", ['$scope', '$http', 'CollectionService', function($scope, $http, CollectionService) {
     $scope.records = [];
-
     $scope.$watch(function() {
         return CollectionService.collection.records;
     }, function(newValue, oldValue, scope) {
@@ -52,5 +51,44 @@ angular.module("recordApp")
             $scope.records = newValue;
         }
     }, true);
+
+    // Sorting
+    $scope.sort_order = "";
+    $scope.sort_title = function() { $scope.sort_order = "title"; };
+    $scope.sort_rating = function() { $scope.sort_order = "rating"; };
+    $scope.sort_year = function() { $scope.sort_order = "year"; };
+
+    $scope.filter_text = "";
+    $scope.filters = {
+        to_download: false,
+        unrated: false,
+    };
+    $scope.filter_records = function(record, index, array) {
+
+        if ($scope.filters.to_download && (!record.to_download || record.downloaded)) return false;
+        if ($scope.filters.unrated && record.rating != undefined) return false;
+
+        if ($scope.filter_text) {
+            var search = $scope.filter_text;
+            var search_terms = search.split(" ");
+            for (var i=0; i<search_terms.length; i++) {
+                var search_term = search_terms[i].toUpperCase();
+                var artist_match = false;
+                for (var artistIndex=0; artistIndex<record.artists.length; artistIndex++) {
+                    if (record.artists[artistIndex].name.toUpperCase().indexOf(search_term)!=-1) {
+                        artist_match = true;
+                        break;
+                    }
+                }
+                return (
+                    (record.title.toUpperCase().indexOf(search_term)!=-1) ||
+                    (record.year.toString().toUpperCase().indexOf(search_term)!=-1) ||
+                    artist_match
+                );
+            }
+        }
+        return true;
+    };
+
 
 }]);
