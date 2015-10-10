d57798199f83736996b02f60f7c3f276dc04de65
chriswait91 <chriswait91@gmail.com>
2015-10-10 13:48:51 +0000

    Adds debugging for runserver

--- server/collection/settings.py
+++ server/collection/settings.py
@@ -108,3 +108,20 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ],
 }
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'DEBUG',
+        }
+    },
+}
