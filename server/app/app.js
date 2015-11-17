c62326ff3c0be591ff8b6d995bbb2b6b100656cf
chriswait <chriswait91@gmail.com>
2015-11-17 23:00:18 +1100

    playing with theming

--- server/app/app.js
+++ server/app/app.js
@@ -1,4 +1,14 @@
 var app = angular.module("recordApp", ['templates','ngMaterial','ngMdIcons']);
-app.controller("MainController", ["CollectionService", function(CollectionService) {
+app.controller("MainController", ["CollectionService", function(CollectionService, $mdThemingProvider) {
     CollectionService.load_collection();
-}]);
+}])
+.config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+    .primaryPalette('brown')
+    .accentPalette('deep-orange')
+    ;
+/*
+    .warnPalette('')
+    .backgroundPalette('')
+*/
+});
